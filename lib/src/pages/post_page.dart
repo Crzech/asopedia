12b3bd9f48c78344f6c5524fe7ee9a08b3c47ff2
Christian Pernillo <cpernillo11@gmail.com>
2021-07-22 09:32:00 -0600

    Final changes

--- lib/src/pages/post_page.dart
+++ lib/src/pages/post_page.dart
@@ -1,3 +1,4 @@
+import 'package:asopedia/src/bloc/list/list_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart' as locale;
@@ -12,66 +13,57 @@ import 'package:asopedia/src/widgets/post/post_content.dart';
 import 'package:asopedia/src/widgets/post/post_header.dart';
 
 class PostPageArguments {
+  const PostPageArguments(
+      {@required this.post, this.comesFromFavorites = false});
 
-  const PostPageArguments({ 
-    @required this.post, 
-    this.comesFromFavorites = false 
-  });
-  
   final AbstractPost post;
   final bool comesFromFavorites;
 }
 
 class PostPage extends StatefulWidget {
-
   @override
   _PostPageState createState() => _PostPageState();
 }
 
 class _PostPageState extends State<PostPage> {
-
-  
   @override
-  void initState() { 
+  void initState() {
     super.initState();
     locale.initializeDateFormatting();
   }
 
   bool _addedToFavorites = false;
 
-  Map<String, dynamic> getFloatingIconButton(addedToFavorites, comesFromFavorites, AbstractPost post, FavoritesCubit favCubit) {
+  Map<String, dynamic> getFloatingIconButton(addedToFavorites,
+      comesFromFavorites, AbstractPost post, FavoritesCubit favCubit) {
     final defaultMethod = () {
-       PostService
-        .addToFavorites(post)
-        .then((bool added) {
-          final _snackBar = SnackBar(content: Text('Favoritos actualizados correctamente'));
-          this.setState(() {
-            _addedToFavorites = true;              
-          });
-          ScaffoldMessenger.of(context).showSnackBar(_snackBar);
-        })
-        .catchError((error) {
-          final _snackBar = SnackBar(content: Text(error.toString()));
-          ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+      PostService.addToFavorites(post).then((bool added) {
+        final _snackBar =
+            SnackBar(content: Text('Favoritos actualizados correctamente'));
+        this.setState(() {
+          _addedToFavorites = true;
         });
+        ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+      }).catchError((error) {
+        final _snackBar = SnackBar(content: Text(error.toString()));
+        ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+      });
     };
     if (comesFromFavorites) {
       return {
         'icon': Icons.bookmark,
         'color': Color(0xffff5252),
         'method': () => {
-          PostService
-            .removeFromFavorites(post)
-            .then((bool added) {
-              favCubit.refreshFavorites();
-              final _snackBar = SnackBar(content: Text('Favoritos actualizados correctamente'));
-              ScaffoldMessenger.of(context).showSnackBar(_snackBar);
-            })
-            .catchError((error) {
-              final _snackBar = SnackBar(content: Text(error.toString()));
-              ScaffoldMessenger.of(context).showSnackBar(_snackBar);
-            })
-        },
+              PostService.removeFromFavorites(post).then((bool added) {
+                favCubit.refreshFavorites();
+                final _snackBar = SnackBar(
+                    content: Text('Favoritos actualizados correctamente'));
+                ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+              }).catchError((error) {
+                final _snackBar = SnackBar(content: Text(error.toString()));
+                ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+              })
+            },
         'tooltip': 'Eliminar de favoritos'
       };
     } else if (_addedToFavorites) {
@@ -90,14 +82,13 @@ class _PostPageState extends State<PostPage> {
     };
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context).settings.arguments as PostPageArguments;
     final screenSize = MediaQuery.of(context).size;
     final _favCubit = BlocProvider.of<FavoritesCubit>(context);
-    final _buttonData = getFloatingIconButton(_addedToFavorites, args.comesFromFavorites, args.post, _favCubit);
+    final _buttonData = getFloatingIconButton(
+        _addedToFavorites, args.comesFromFavorites, args.post, _favCubit);
     final _dateFormat = DateFormat.yMMMMd('es_ES');
     return Scaffold(
       body: FutureBuilder<GlossaryPost>(
@@ -108,16 +99,15 @@ class _PostPageState extends State<PostPage> {
             return CustomScrollView(
               slivers: [
                 PostHeader(
-                  screenSize: screenSize,  
-                  title: snapshot.data.title.rendered,
-                  datetime: snapshot.data.date,
-                  embedeData: snapshot.data.embedeData
-                ),
+                    screenSize: screenSize,
+                    title: snapshot.data.title.rendered,
+                    datetime: snapshot.data.date,
+                    embedeData: snapshot.data.embedeData),
                 SliverToBoxAdapter(
                   child: Padding(
                     padding: const EdgeInsets.only(top: 20.0, left: 28),
                     child: Text(
-                      snapshot.data.title.rendered, 
+                      snapshot.data.title.rendered,
                       style: TextStyle(
                         height: 1,
                         color: ThemeManager.getPrimaryColor(),
@@ -127,16 +117,19 @@ class _PostPageState extends State<PostPage> {
                     ),
                   ),
                 ),
-                SliverToBoxAdapter(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 28.0, vertical: 10.0),
-                    child: Text(
-                      _dateFormat.format(snapshot.data.date),
-                      style: TextStyle(
-                        fontSize: 18.0
+                BlocBuilder<ListCubit, ListState>(
+                  builder: (context, state) {
+                    return state.slug != 'eventos' ? SliverToBoxAdapter(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 28.0, vertical: 10.0),
+                        child: Text(
+                          _dateFormat.format(snapshot.data.date),
+                          style: TextStyle(fontSize: 18.0),
+                        ),
                       ),
-                    ),
-                  ),
+                    ) : SliverToBoxAdapter(child: SizedBox(height: 0));
+                  },
                 ),
                 PostContent(content: snapshot.data.content)
               ],
@@ -147,11 +140,10 @@ class _PostPageState extends State<PostPage> {
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Center(
-                child: Text(
-                  'Ha ocurrido un error inesperado, por favor cierre sesión y inténte de nuevo',
-                  textAlign: TextAlign.center,
-                )
-              ),
+                  child: Text(
+                'Ha ocurrido un error inesperado, por favor cierre sesión y inténte de nuevo',
+                textAlign: TextAlign.center,
+              )),
             );
           }
           return Center(child: CircularProgressIndicator());
